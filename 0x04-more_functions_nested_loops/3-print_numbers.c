538a542b4938269c95f72136fb3e95f473778b95
Developer-in-Tech <princeakinola05@gmail.com>
2022-10-22 00:17:45 -0700

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
-/*
- * print_numbers - prints number
- */
+
+/* print_numbers - prints number */
+
+
 
 void print_numbers(void)
+
 {
+
 	int i;
 
 	for (i = 48; i < 58; i++)
+
 	{
+
 		_putchar(i);
+
 	}
-	_putchar("\n");
+
+	_putchar('\n');
+
 }
