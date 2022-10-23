9be4a88f1f875ceff18d23edbde34467bd8c6cff
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-10-23 11:34:52 +0100

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 	char d;
 
 
-	for (c = a, c <= z, c++)
+	for (c = "a", c <= "z", c++)
 		putchar(c);
 
-	for (d = A, d <= Z, d++)
+	for (d = "A", d <= "Z", d++)
 		putchar(d);
 
 	putchar("\n");
