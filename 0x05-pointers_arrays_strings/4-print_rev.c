0508e9d419a3b49de34e2bb08dee6301eda97d7f
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-10-25 19:08:08 +0100

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * print_rev - The function print a string in reversed order
+ * print_rev - This function prints a string in reverse order
  * @s: pointer parameter
  */
 
 void print_rev(char *s)
 {
 	int i = 0;
+	int j;
 
-	while (s[i] != "\0")
+	while (s[i] != '\0')
+	{
 		i++;
-	for (i = i - 1; 1 >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar("\n")
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }
