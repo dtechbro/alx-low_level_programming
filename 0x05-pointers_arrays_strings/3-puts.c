f861e5d1bc72ea10bc432a44719ca5203579b65e
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-10-25 18:12:32 +0100

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _puts -  print string to stdout
  * @str: pointer parameter
@@ -9,13 +8,11 @@ void _puts(char *str)
 {
 	int i = 0;
 
-		while (str[i] != '\0')
-		{
+	while (str[i] != "\0")
+	{
+	_putchar(str[i]);
+	i++;
+	}
 
-		_putchar(str[i]);
-		i++;
-		}
-
-
-			_putchar("\n");
+	_putchar("\n");
 }
