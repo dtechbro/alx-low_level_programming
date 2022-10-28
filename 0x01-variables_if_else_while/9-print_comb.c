9a2a1efd8ad3da8cfe35d46c1506263f13fe5d7f
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-10-29 00:17:33 +0100

    combine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int i;
 
-	for ( i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i);
-		if ( i != 9)
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar(i);
-	return: 0;
+	return: (0);
 }
