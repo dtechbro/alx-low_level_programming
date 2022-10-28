87127a5d639e53c8abfd41dab6b08acf03a67942
Developer-in-Tech <princeakinola05@gmail.com>
2022-10-28 15:58:02 -0700

    combine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for ( i = 0; i < 10; i++)
+	{
+		putchar(i);
+		if ( i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(i);
+	return: 0;
+}
