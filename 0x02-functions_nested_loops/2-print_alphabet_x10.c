170a2bc2275741bdf592e4f711ec453afbc6f0f5
Developer-in-Tech <princeakinola05@gmail.com>
2022-10-29 02:20:59 -0700

    a-z X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n')
+	}
+}
