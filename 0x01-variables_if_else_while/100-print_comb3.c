9687f5383637657b1252d20d7cbeec854c1be768
Developer-in-Tech <princeakinola05@gmail.com>
2022-10-30 09:40:49 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
+
 /**
  * main - program compilation begins from main
- * Description: Program to print all possible combinations of numbers
- with no two numbers same in pairing
- * author: sammykingx
  * Return: 0 means success
  */
 int main(void)
 {
-        int a, b;
+	int i;
+	int j;
 
-        for (a = 48; a <=57; a++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        if ( a != b && b > a)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                putchar(',');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (i = 48; i <=57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (i != j && j > i)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+			}
+		
+		}
+	}
+	putchar('\n');
+	return (0);
 }
