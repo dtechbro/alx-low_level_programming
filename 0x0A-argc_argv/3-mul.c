f8dad6bc798fc468735ba3ce15f925bd8fa9dead
Developer-in-Tech <princeakinola05@gmail.com>
2022-11-06 15:53:35 -0800

    multiplication of integers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints multiplication of integers
+ * @argc: Counts argument
+ * @argv: Argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+
+	printf("%i\n", c);
+	return (0);
+}
