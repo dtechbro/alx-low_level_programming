67de33376f11d9bad736274eb4f630001810474c
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-11-10 02:00:08 +0100

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,29 @@
-nbjjblbnlmfklifnmf , m
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main funct
+ * @argc: counts argument
+ * @argv: Argument
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+  int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
