55e20731d9d10b3ee2037a40355cdf56dd904ee9
Developer-in-Tech <princeakinola05@gmail.com>
2022-11-27 16:25:17 -0800

    print 1-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print 1 to 98
+ * @n: Integer
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+
+	printf("98");
+	putchar('\n');
+}
