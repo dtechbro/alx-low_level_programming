16d6b28e5e802353f3074e4e65fdea3b5219d7e3
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-12-04 18:29:17 +0100

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,11 @@ int main(void)
 {
 	int a = 0, b = 0;
 	
-	
-	for (a = 0; a < 1024; a++)
-	{
-		if ((a % 5) == 0 || (a % 3) == 0)
-			b += a;
-	}
+		for (a = 0; a < 1024; a++)
+		{
+			if ((a % 5) == 0 || (a % 3) == 0)
+				b += a;
+		}
 	printf("%d\n", b);
 
 	return (0);
