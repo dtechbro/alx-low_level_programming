c7aba533c44ba2c4f1260d8a9f7ff692a3395cd9
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-12-04 18:13:15 +0100

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+  int a = 0;
+  int b = 0;
+  
+  for (a = 0; a < 1024; a++)
+  {
+    if ((a % 5) == 0 || (a % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
