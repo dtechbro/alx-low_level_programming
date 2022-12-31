bb0d6c77250f8d4ec98d39c9f55932fd42579d5c
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2022-12-31 15:22:16 +0100

    file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,28 +13,17 @@ int main(void) {
 
 	// If the current term is even, add it to the sum
 	if (b % 2 == 0) {
-
 		sum += b;
 	}
+	
+	// Generate the next term in the sequence by adding the previous two terms
+	int c = a + b;
 
-    // Generate the next term in the sequence by adding the previous two terms
-
-    int c = a + b;
-
-    // Shift the terms forward so that a becomes b and b becomes c
-
-    a = b;
-
-    b = c;
-
-  }
-
-
-
-  // Print the sum of the even-valued terms
-
-  printf("%d\n", sum);
-
-  return 0;
-
+	// Shift the terms forward so that a becomes b and b becomes c
+	a = b;
+	b = c;
+}
+	// Print the sum of the even-valued terms
+	printf("%d\n", sum);
+	return 0;
 }
