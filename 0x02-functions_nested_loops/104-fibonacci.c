0b830e9b8cbcbdc05aff61ca61f98f06616ecb3b
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2023-01-16 16:30:49 +0100

    Update 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int a = 1, b = 2, c;
+    printf("%d, %d", a, b);
+    for (i = 2; i < 98; i++) {
+        c = a + b;
+        printf(", %d", c);
+        a = b;
+        b = c;
+    }
+    printf("\n");
+    return 0;
+}
