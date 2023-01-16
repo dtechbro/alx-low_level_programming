3acc288c00d1e2856109e33eda86b95befff53d1
Muhammad-Nabeel <93738997+Developer-in-Tech@users.noreply.github.com>
2023-01-16 01:40:18 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int a = 1, b = 2, c, sum = 0;
     
-    while (b <= 4000000) {
-        if (b % 2 == 0) {
+    while (b <= 4000000)
+    {
+        if (b % 2 == 0)
+        {
             sum += b;
         }
         c = a + b;
