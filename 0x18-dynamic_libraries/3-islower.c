fcdf47ded4a4caed45abd2a859aa6a546a54bfa3
Developer-in-Tech <princeakinola05@gmail.com>
2023-01-30 13:27:11 -0800

    file

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - checks for lower case letters
+ * @c: character
+ * Return: 0 on success
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
